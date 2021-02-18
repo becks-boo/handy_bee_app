d541d94e353231f448cea4b01280119321504973
Lena Blank <mine.blank@gmail.com>
2021-02-18 21:18:12 +0100

    navbar, avatar and btn

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,10 +1,10 @@
 class BusinessesController < ApplicationController
   def index
     # @businesses = Business.all
-    @businesses = Business.search(params[:search])
+    @business = Business.search(params[:search])
   end
 
-  # def show
-  #   @businesses = Business.find(params[:id])
-  # end
+  def show
+    @business = Business.find(params[:id])
+  end
 end
