91bec61477d1007ac5322591325e4e81a08c84b5
Igor Stein <igor_stein@yahoo.de>
2021-02-18 20:09:17 +0100

    simple create method, view no form yet, pundit has to be done now

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,17 +8,22 @@ class BookingsController < ApplicationController
   end
 
   def create
+    # initialize new booking
     @booking = Booking.new(booking_params)
     @booking.user = current_user
 
     @booking.business = @business
 
-    if @booking
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    else
+      render "businesses/show"
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:price, :start_date, :end_date)
   end
 end
