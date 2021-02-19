ae137fd3b81189160192b0fc74394fcde9d6316d
Igor Stein <igor_stein@yahoo.de>
2021-02-19 18:48:05 +0100

    create review model and the associations

--- app/models/business.rb
+++ app/models/business.rb
@@ -4,7 +4,7 @@ class Business < ApplicationRecord
   # UNCOMMENT when creating these tables
 
    # has_many :bookings, dependent: :destroy
-   # has_many :reviews, dependent: :destroy
+   has_many :reviews, dependent: :destroy
    has_many_attached :pictures
 
    validates :name, :description, :category, presence: true
