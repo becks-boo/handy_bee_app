c4d391065ad6a1d2de73b2ed42d34137050993f8
Xavi Molina <xavimolina90@gmail.com>
2021-02-20 11:44:14 +0100

    pundit index and show businesses controller

--- db/schema.rb
+++ db/schema.rb
@@ -87,12 +87,6 @@ ActiveRecord::Schema.define(version: 2021_02_19_181036) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -106,10 +100,8 @@ ActiveRecord::Schema.define(version: 2021_02_19_181036) do
     t.string "picture"
     t.string "role"
     t.text "qualifications"
-    t.bigint "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -120,5 +112,4 @@ ActiveRecord::Schema.define(version: 2021_02_19_181036) do
   add_foreign_key "messages", "users"
   add_foreign_key "reviews", "businesses"
   add_foreign_key "reviews", "users"
-  add_foreign_key "users", "roles"
 end
