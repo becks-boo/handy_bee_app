e755ca8ccdbd5368977eafa57d0de88ecff79eec
Igor Stein <igor_stein@yahoo.de>
2021-02-20 14:52:42 +0100

    validate review

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,8 +1,6 @@
 class Business < ApplicationRecord
    belongs_to :user
 
-  # UNCOMMENT when creating these tables
-
    has_many :bookings, dependent: :destroy
    has_many :reviews, through: :bookings, dependent: :destroy
    has_many_attached :pictures
