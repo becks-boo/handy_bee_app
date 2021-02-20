8c44a99a88fe36744e2e998578e386ea0fc66d87
jordannadroj <jordansiemens93@gmail.com>
2021-02-20 16:46:52 +0100

    fixed action able

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,9 @@ ActiveRecord::Schema.define(version: 2021_02_20_134924) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "qualification"
+    t.string "location"
+    t.string "language"
     t.index ["user_id"], name: "index_businesses_on_user_id"
   end
 
@@ -87,12 +90,6 @@ ActiveRecord::Schema.define(version: 2021_02_20_134924) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -104,10 +101,8 @@ ActiveRecord::Schema.define(version: 2021_02_20_134924) do
     t.string "user_name"
     t.string "picture"
     t.string "role"
-    t.bigint "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -118,5 +113,4 @@ ActiveRecord::Schema.define(version: 2021_02_20_134924) do
   add_foreign_key "messages", "users"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "reviews", "users"
-  add_foreign_key "users", "roles"
 end
