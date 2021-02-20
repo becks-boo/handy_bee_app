14fbf4c0d819ea663183293e577992e4b9c5dd04
Xavi Molina <xavimolina90@gmail.com>
2021-02-20 10:21:20 +0100

    working on front end business index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_02_16_190256) do
 ActiveRecord::Schema.define(version: 2021_02_16_195115) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
