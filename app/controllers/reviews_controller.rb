7f3dc5bb730eec0c0245c5074e96e3aca83830a7
Lena Blank <mine.blank@gmail.com>
2021-02-21 17:23:02 +0100

    reviews controller all actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+  before_action :set_booking, except: [:edit, :update, :destroy]
+  before_action :set_review, only: [:edit, :update, :destroy]
+
   def new
     @review = Review.new
     authorize @review
@@ -17,10 +20,37 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    authorize @review
+  end
+
+  def update
+    authorize @review
+    if @review.update(review_params)
+      redirect_to booking_path(@review.booking)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    authorize @review
+    redirect_to booking_path(@review.booking)
+  end
+
   private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
   end
-
 end
