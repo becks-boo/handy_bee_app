60bc9044d9defba24dfd38781ae9d7dffe168342
Igor Stein <igor_stein@yahoo.de>
2021-02-21 17:43:02 +0100

    create new business form and create method

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -16,4 +16,22 @@ class BusinessesController < ApplicationController
     @business = Business.new
     authorize @business
   end
+
+  def create
+    @business = Business.new(business_params)
+    @business.user = current_user
+    authorize @business
+
+    if @business.save
+      redirect_to business_path(@business)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :description, :picture, :category, :qualification, :location, :language)
+  end
 end
