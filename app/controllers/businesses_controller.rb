526aa4c607cf0aa15cb0e031ff0fe71fcee9610a
Xavi Molina <xavimolina90@gmail.com>
2021-02-23 20:23:24 +0100

    Filter by category + business coount

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,18 +3,18 @@ class BusinessesController < ApplicationController
     # @businesses = Business.all
     # @businesses = Business.search(params[:search])
     @businesses = policy_scope(Business)
+    # filter by category
     if params[:query].present?
       @businesses = Business.where(category: params[:query])
+      @businesses_count = @businesses.count
     else
       @businesses = Business.all
     end
   end
-
 
   def show
     @business = Business.find(params[:id])
     @bookings = @business.bookings
     authorize @business
   end
-
 end
