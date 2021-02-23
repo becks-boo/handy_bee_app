97677e2474c9a4e65c0b5aab6efaeef61634da2d
Xavi Molina <xavimolina90@gmail.com>
2021-02-23 19:17:57 +0100

    trying dropdown

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,8 +1,13 @@
 class BusinessesController < ApplicationController
   def index
-    @businesses = Business.all
+    # @businesses = Business.all
     # @businesses = Business.search(params[:search])
     @businesses = policy_scope(Business)
+    if params[:query].present?
+      @businesses = Business.where(category: params[:query])
+    else
+      @businesses = Business.all
+    end
   end
 
 
