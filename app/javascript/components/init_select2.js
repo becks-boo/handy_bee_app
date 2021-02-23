c31b2a0b409bc654880d93949fe483c8329bf630
jordannadroj <jordansiemens93@gmail.com>
2021-02-23 23:38:24 +0100

    homepage dropdown

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.custom-select').select2(
-    theme: 'bootstrap'
+  $('.select2').select2(
+    theme: "classic"
     // it is not working properly with theme bootstrap, however the default select 2 theme is ugly
     );
 };
