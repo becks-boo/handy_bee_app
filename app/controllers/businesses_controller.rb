3a7f2172c7796af3172c51812b6e7d60bfd8a2b6
Xavi Molina <xavimolina90@gmail.com>
2021-02-23 18:55:25 +0100

    nothing changed

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -7,17 +7,9 @@ class BusinessesController < ApplicationController
 
 
   def show
-<<<<<<< HEAD
-    puts "Holisito k tal"
-=======
-    # raise
->>>>>>> main
     @business = Business.find(params[:id])
     @bookings = @business.bookings
     authorize @business
   end
 
-  def category
-    @business = Business.find(params[:category])
-  end
 end
