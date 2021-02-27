bc647dca885a72374d33277725291f3fcf16720c
Igor Stein <igor_stein@yahoo.de>
2021-02-27 17:56:44 +0100

    start to add methods in language controller

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,2 +1,22 @@
 class LanguagesController < ApplicationController
+  # Do we need the index method?
+  # def index
+  #   @languages = Language.all
+  # end
+
+  def new
+    @language = Language.new
+  end
+
+  def create
+    @language = Language.new(language_params)
+    authorize @language
+    # Not finished
+  end
+
+  private
+
+  def language_params
+    params.require(:language).permit(:name)
+  end
 end
