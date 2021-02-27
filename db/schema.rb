9e0ddd98c7000da7b2ba1a89a42e154cacdaec5a
Igor Stein <igor_stein@yahoo.de>
2021-02-27 17:02:30 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_110327) do
+ActiveRecord::Schema.define(version: 2021_02_27_151141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2021_02_27_110327) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "business_languages", force: :cascade do |t|
+    t.bigint "language_id", null: false
+    t.bigint "business_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["business_id"], name: "index_business_languages_on_business_id"
+    t.index ["language_id"], name: "index_business_languages_on_language_id"
+  end
+
   create_table "businesses", force: :cascade do |t|
     t.string "name"
     t.string "picture"
@@ -77,6 +86,12 @@ ActiveRecord::Schema.define(version: 2021_02_27_110327) do
     t.index ["business_id"], name: "index_chatrooms_on_business_id"
     t.index ["contractor_id"], name: "index_chatrooms_on_contractor_id"
     t.index ["customer_id"], name: "index_chatrooms_on_customer_id"
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -120,6 +135,8 @@ ActiveRecord::Schema.define(version: 2021_02_27_110327) do
   add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "users", column: "contractor_id"
   add_foreign_key "bookings", "users", column: "customer_id"
+  add_foreign_key "business_languages", "businesses"
+  add_foreign_key "business_languages", "languages"
   add_foreign_key "businesses", "users"
   add_foreign_key "chatrooms", "businesses"
   add_foreign_key "chatrooms", "users", column: "contractor_id"
