7ecad514d05bc084cdde924b1dd3f1dff675e552
jordannadroj <jordansiemens93@gmail.com>
2021-03-02 21:14:21 +0100

    improving filters

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -6,17 +6,21 @@ class BusinessesController < ApplicationController
     # filter by category
     if params[:query].present?
       @businesses = Business.where(category: params[:query])
-      @businesses_count = @businesses.count
+      # @businesses_count = @businesses.count
     else
       @businesses = Business.all
     end
 
-    if params[:filter].present?
-      @businesses = Business.where(language: params[:filter])
-      @businesses_count = @businesses.count
+    if params[:language].present?
+      @businesses = Business.where(language: params[:language])
+      # @businesses_count = @businesses.count
     end
 
-    filter_language
+    if params[:rating].present?
+      @businesses = Business.includes(:reviews).where("reviews.rating" => params[:rating])
+    end
+
+    # filter_language
   end
 
   def show
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :business
+  has_one :business, through: :booking
   belongs_to :booking
 
   validates :content, :rating, presence: true
