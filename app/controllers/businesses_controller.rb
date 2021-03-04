aaf63b64a1f2109fcd4f6915a03a0a5f7c6eab74
jordannadroj <jordansiemens93@gmail.com>
2021-03-04 19:55:03 +0100

    able to chain search and filters

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -6,19 +6,21 @@ class BusinessesController < ApplicationController
     # filter by category
     if params[:query].present?
       @businesses = Business.where(category: params[:query])
-      # @businesses_count = @businesses.count
+      if params[:language].present? && params[:rating].present?
+        @businesses = Business.where(language: params[:language], category: params[:query]).includes(:reviews).where("reviews.rating" => params[:rating])
+      elsif params[:language].present?
+        @businesses = Business.where(language: params[:language], category: params[:query])
+      elsif params[:rating].present?
+        @businesses = Business.includes(:reviews).where("reviews.rating" => params[:rating])
+      end
     else
       @businesses = Business.all
     end
 
-    if params[:language].present?
-      @businesses = Business.where(language: params[:language])
-      # @businesses_count = @businesses.count
-    end
 
-    if params[:rating].present?
-      @businesses = Business.includes(:reviews).where("reviews.rating" => params[:rating])
-    end
+    # if params[:rating].present?
+    #   @businesses = Business.includes(:reviews).where("reviews.rating" => params[:rating])
+    # end
 
     # filter_language
   end
