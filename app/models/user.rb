a9726c65e1511a8952a3993e003719355517471e
Igor Stein <igor_stein@yahoo.de>
2021-03-06 11:47:49 +0100

    add business_language to seed, seeded business have languages now, global variable for category, update show and index page, add dependent destroy to messages and chatrooms

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_many :businesses, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  has_many :messages
-  has_many :chatrooms_as_customer, foreign_key: :customer_id, class_name: :Chatroom
-  has_many :chatrooms_as_contractor, foreign_key: :contractor_id, class_name: :Chatroom
+  has_many :messages, dependent: :destroy
+  has_many :chatrooms_as_customer, foreign_key: :customer_id, class_name: :Chatroom, dependent: :destroy
+  has_many :chatrooms_as_contractor, foreign_key: :contractor_id, class_name: :Chatroom, dependent: :destroy
 
   has_one_attached :picture
 
