775330eed63b21b936696438670fe85c58feb599
jordannadroj <jordansiemens93@gmail.com>
2021-03-08 21:01:13 +0100

    fixing migrations

--- db/migrate/20210308192744_remove_price_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemovePriceFromBookings < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :bookings, :price_cents, :integer
-  end
-end
--- db/migrate/20210308192833_add_price_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToBookings < ActiveRecord::Migration[6.0]
-  def change
-    add_column :bookings, :price, :integer
-  end
-end
