e6ab6e58474520e235f749b4b159f8d5d251a99e
jordannadroj <jordansiemens93@gmail.com>
2021-04-11 15:47:11 +0200

    write first unit and system test
    unit test tests is user.user_name matches username when created. System test test what the user sees when going to index businesses page

--- test/system/businesses_test.rb
+++ test/system/businesses_test.rb
@@ -1,10 +1,15 @@
 require "application_system_test_case"
 
 class BusinessesTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit businesses_url
+  # test "visiting the index" do
+  #   visit root_url
 
+  #   # save_and_open_screenshot
+  #   assert_selector "h1", text: "Fix your house"
+  # end
+  test "lets a signed in user view all bussiness" do
+    login_as users(:george)
+    visit "/businesses"
     save_and_open_screenshot
-    assert_selector "h1", text: "Fix your house"
   end
 end
